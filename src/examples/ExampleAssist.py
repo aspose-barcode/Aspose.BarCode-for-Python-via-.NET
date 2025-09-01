d392b2868763e93167f9e5a47b707f3b71b3f38d
Denis Averin <denis.averin@aspose.com>
2025-09-01 21:37:28 +0700

    Fixing paths in ExampleAssist.py

--- src/examples/ExampleAssist.py
+++ src/examples/ExampleAssist.py
@@ -1,9 +1,12 @@
 import os
 import aspose.barcode
 
-pythonLicensePath = "../resources/license/Aspose.BarCode.Python.NET.lic"
-results_root = "../resources/generated/"
-test_data_root = "../resources/input/"
+_FILE_DIR = os.path.dirname(__file__)
+
+pythonLicensePath = os.path.join(_FILE_DIR, "..", "resources", "license", "Aspose.BarCode.Python.NET.lic")
+results_root = os.path.join(_FILE_DIR, "..", "resources", "generated") + os.sep
+test_data_root = os.path.join(_FILE_DIR, "..", "resources", "input") + os.sep
+
 
 def set_license():
     if (os.path.exists(pythonLicensePath)):
@@ -15,8 +18,4 @@ def set_license():
             print(e)
             print("License was not installed.")
     else:
-            print("Path " + pythonLicensePath + " is not correct\nLicense was not installed.")
-
-
-
-
+        print("Path " + pythonLicensePath + " is not correct\nLicense was not installed.")
